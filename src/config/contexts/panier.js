e331fdea041a63911e9455f6de1d61c06dcb6c94
Lamine ADEL <lamine.adel@ynov.com>
2022-03-28 01:55:10 +0200

    AddPrice

--- src/config/contexts/panier.js
+++ src/config/contexts/panier.js
@@ -6,31 +6,44 @@ const Panier = createContext({
     cart: [],
     setCart: () => {},
     addorremove: () => {},
+    price: 0,
+    setPrice: () => {},
 })
 
 export const PanierProvider = ({ children }) => {
     const [cart, setCart] = useState([])
-
-    const verfyCart = async () => {
-        const Carts = await AsyncStorage.getItem("Panier")
-        Carts && setCart(JSON.parse(Carts))
-    }
+    const [price, setPrice] = useState(0)
 
     useEffect(() => {
+        const verfyCart = async () => {
+            const Carts = await AsyncStorage.getItem("Panier")
+            Carts.length > 0 && setCart(JSON.parse(Carts))
+        }
         verfyCart()
     }, [])
+
+    useEffect(() => {
+        setPrice(
+            cart.reduce(
+                (total, currentValue) => (total = total + currentValue.estimatedMarketValue),
+                0,
+            ),
+        )
+    }, [cart])
 
     const addorremove = (item) => {
         const existe = cart.findIndex((element) => element.id === item.id)
 
         if (existe !== -1) {
             setCart(cart.filter((element) => element.id !== item.id))
+            setPrice(price - item.estimatedMarketValue)
             showMessage({
                 message: "PRODUIT SUPPRIMÉ DU PANIER",
                 type: "danger",
             })
         } else {
             setCart([...cart, item])
+            setPrice(price + item.estimatedMarketValue)
             showMessage({
                 message: "PRODUIT AJOUTÉ AU PANIER",
                 type: "success",
@@ -44,6 +57,8 @@ export const PanierProvider = ({ children }) => {
     const context = {
         cart,
         setCart,
+        price,
+        setPrice,
         addorremove,
     }
     return <Panier.Provider value={context}>{children}</Panier.Provider>
