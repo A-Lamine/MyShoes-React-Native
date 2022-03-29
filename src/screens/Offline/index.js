a86ea357ef9a84979d7f0532bbf4c4cd7cd2270f
Lamine ADEL <lamine.adel@ynov.com>
2022-03-29 03:29:52 +0200

    AddOfflineScreen

---
+++ src/screens/Offline/index.js
@@ -0,0 +1,37 @@
+import React from "react"
+import styled from "styled-components"
+import Offiline_LOGO from "../../../public/no-signal.png"
+const Index = () => {
+    return (
+        <Div>
+            <Img source={Offiline_LOGO} />
+            <H1>Vous êtes hors ligne</H1>
+            <H1>Merci de verifier votre connexion internet</H1>
+        </Div>
+    )
+}
+
+const Div = styled.View`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: #34495e;
+    justify-content: center;
+`
+
+const Img = styled.Image`
+    width: 40%;
+    height: 20%;
+    border-radius: 20px;
+`
+const H1 = styled.Text`
+    font-size: 30px;
+    color: black;
+    font-weight: bold;
+    padding: 5px;
+    text-align: center;
+`
+
+export default Index
